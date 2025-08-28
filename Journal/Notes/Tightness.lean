cd6ddf15c2d58033b0e591607ce1c186e172fef9
pfaffelh <p.p@stochastik.uni-freiburg.de>
2025-08-28 23:47:51 +0200

    added WRT

--- Journal/Notes/Tightness.lean
+++ Journal/Notes/Tightness.lean
@@ -3,57 +3,93 @@ import Mathlib
 open Set Filter ENNReal NNReal TopologicalSpace
 open scoped symmDiff Topology
 
-section atTopOf
+section atTopAbove
 
-def atTopOf [LE α] (p : α → Prop) : Filter α :=
-  ⨅ K ∈ {K' | p K'}, 𝓟 {B | K ≤ B}
+variable {α : Type*} [Preorder α] (p : α → Prop)
 
-lemma mem_atTopOf_iff_of_directed [Preorder α] (p : α → Prop) (h₀ : {K' | p K'}.Nonempty) (h₁ : DirectedOn (fun x y ↦ x ≤ y) p) : s ∈ atTopOf p ↔ ∃ x ∈ {y | p y}, s ∈ 𝓟 {b | x ≤ b} := by
-  simp only [atTopOf]
-  rw [mem_biInf_of_directed _ h₀]
-  refine directedOn_image.mp ?_
-  simp only [DirectedOn] at h₁ ⊢
-  intro x hx y hy
-  simp only [mem_image, mem_setOf_eq] at hx hy
-  obtain ⟨s, ⟨hs1, hs2⟩⟩ := hx
-  obtain ⟨t, ⟨ht1, ht2⟩⟩ := hy
-  simp only [mem_image, mem_setOf_eq, ge_iff_le, exists_exists_and_eq_and]
-  obtain ⟨u, hu1, hu2, hu3⟩ := h₁ s hs1 t ht1
-  use u
-  refine ⟨hu1, ?_, ?_⟩
-  · rw [← hs2]
-    simp only [le_principal_iff, mem_principal, setOf_subset_setOf]
-    intro a ha
-    apply le_trans hu2 ha
-  · rw [← ht2]
-    simp only [le_principal_iff, mem_principal, setOf_subset_setOf]
-    intro a ha
-    apply le_trans hu3 ha
+def atTopAbove : Filter α :=
+  ⨅ (a : α) (_ : p a), Filter.principal (Set.Ici a)
+
+lemma atTopAbove_eq : atTopAbove p = ⨅ a ∈ {x | p x}, Filter.principal (Set.Ici a) := by rfl
+
+lemma mem_atTopAbove_iff_of_directed {s : Set α} (h₀ : {y | p y}.Nonempty) (h₁ : DirectedOn (fun x y ↦ x ≤ y) p) : s ∈ atTopAbove p ↔ ∃ (x : α) (_ : p x), s ∈ Filter.principal (Set.Ici x) := by
+  rw [atTopAbove_eq, mem_biInf_of_directed _ h₀]
+  · exact Iff.symm bex_def
+  · rw [← directedOn_image]
+    intro x hx y hy
+    obtain ⟨s, ⟨hs1, hs2⟩⟩ := hx
+    obtain ⟨t, ⟨ht1, ht2⟩⟩ := hy
+    simp only [mem_image, mem_setOf_eq, ge_iff_le, exists_exists_and_eq_and]
+    obtain ⟨u, hu1, hu2, hu3⟩ := h₁ s hs1 t ht1
+    use u
+    refine ⟨hu1, hs2.symm ▸ ?_, ht2.symm ▸ ?_⟩ <;> simp [hu2, hu3]
+
+end atTopAbove
+
+section Set
+
+variable {α : Type*} {p : Set α → Prop}
+
+lemma directedOn_of_unionMem (hp : ∀ (s : Set α) (_ : p s) (t : Set α) (_ : p t), p (s ∪ t)) : DirectedOn (α := Set α) (fun s t ↦ s ⊆ t) p :=
+  fun x hx y hy ↦ ⟨x ∪ y, ⟨hp x hx y hy, union_subset_iff.mp fun ⦃_⦄ a => a⟩⟩
+
+lemma mem_atTopAbove_iff_of_unionMem (a : Set (Set α)) (h : {s : Set α | p s }.Nonempty) (hp : ∀ (s : Set α) (_ : p s) (t : Set α) (_ : p t), p (s ∪ t)) : a ∈ atTopAbove p ↔ ∃ K : Set α, p K ∧ ((Filter.principal K).sets ⊆ a) := by
+  rw [mem_atTopAbove_iff_of_directed p h (directedOn_of_unionMem hp)]
+  simp_all only [mem_principal, exists_prop]
+  rfl
+
+end Set
+
+section isCompact
 
 variable {α : Type*} [TopologicalSpace α]
 
-lemma directedOn_of_isCompact : DirectedOn (α := Set α) (fun s t ↦ s ⊆ t) IsCompact :=
-  fun x hx y hy ↦ ⟨x ∪ y, ⟨IsCompact.union hx hy, union_subset_iff.mp fun ⦃_⦄ a => a⟩⟩
+lemma mem_iInf_of_isCompact (a : Set (Set α)) : a ∈ atTopAbove IsCompact ↔
+    ∃ K : Set α, IsCompact K ∧ ((𝓟 K).sets ⊆ a) :=
+  mem_atTopAbove_iff_of_unionMem a ⟨∅, isCompact_empty⟩ (fun _ hs _ ht => IsCompact.union hs ht)
 
-lemma isCompact_nonempty : {K : Set α| IsCompact K}.Nonempty :=
-  ⟨∅, isCompact_empty⟩
+lemma mem_iInf_of_isCompactSubset (a : Set (Set α)) (t : Set α) : a ∈ atTopAbove
+    (fun s ↦ IsCompact s ∧ s ⊆ t) ↔ ∃ K : Set α, (IsCompact K ∧ K ⊆ t) ∧ ((𝓟 K).sets ⊆ a) :=
+  mem_atTopAbove_iff_of_unionMem a ⟨∅, ⟨isCompact_empty, empty_subset t⟩⟩
+    (fun _ ⟨hs1, hs2⟩ _ ⟨hu1, hu2⟩ ↦ ⟨IsCompact.union hs1 hu1, union_subset hs2 hu2⟩)
 
-lemma mem_iInf_of_isCompact (a : Set (Set α)) : a ∈ atTopOf IsCompact ↔ ∃ K : Set α, IsCompact K ∧ ((𝓟 K).sets ⊆ a) := by
-  rw [mem_atTopOf_iff_of_directed _ isCompact_nonempty directedOn_of_isCompact]
-  simp only [mem_setOf_eq, le_eq_subset, mem_principal]
-  rfl
-
-end atTopOf
+end isCompact
 
 namespace MeasureTheory
 
 namespace Measure
 
+section innerRegular
+
+variable {α : Type*} [TopologicalSpace α] [MeasurableSpace α] (ℙ : Measure α) [IsProbabilityMeasure ℙ]
+
+lemma mem_nhdsIci_iff (x : ℝ≥0∞) : s ∈ nhdsSet (Ici x) ↔ ∃ r < x,  Ioi r ⊆ s := by
+  sorry
+
+lemma innerRegular_iff (p q : Set α → Prop) (h₀ : p ∅) (hp : ∀ (s : Set α) (_ : p s) (t : Set α) (_ : p t), p (s ∪ t)) : ℙ.InnerRegularWRT p q ↔ ∀ (t : Set α) (ht : q t ∧ 0 < ℙ t), Tendsto (fun s ↦ ℙ s) (atTopAbove (fun s ↦ p s ∧ s ⊆ t)) (nhdsSet (Ici (ℙ t))) := by
+  have h₀' (t : Set α): {s | p s ∧ s ⊆ t}.Nonempty := ⟨∅, ⟨h₀, empty_subset t⟩⟩
+  have hp' (u : Set α) : ∀ (s : Set α) (_ : p s ∧ s ⊆ u) (t : Set α) (_ : p t ∧ t ⊆ u), p (s ∪ t) ∧ s ∪ t ⊆ u := fun s ⟨hs1, hs2⟩ t ⟨ht1, ht2⟩ ↦ ⟨hp s hs1 t ht1, union_subset hs2 ht2⟩
+  simp only [InnerRegularWRT]
+  simp_rw [tendsto_iff_forall_eventually_mem, mem_nhdsIci_iff]
+  change _ ↔ (∀ t, q t ∧ ℙ t > 0→ ∀ s, _ → {x | ℙ x ∈ s} ∈ atTopAbove fun s => p s ∧ s ⊆ t)
+  conv => right ; right ; right ; right ; right ;  rw [mem_atTopAbove_iff_of_unionMem _ (h₀' _) (hp' _)]
+  refine ⟨fun h t ht ↦ fun s hs ↦ ?_, fun h t ht r hr ↦ ?_⟩
+  · obtain ⟨r, ⟨hr1, hr2⟩⟩ := hs
+    obtain ⟨K, ⟨hK1, hK2, hK3⟩⟩ := h ht.1 r hr1
+    refine ⟨K, ⟨hK2, hK1⟩, fun L hL ↦ hr2 <| lt_of_lt_of_le hK3 <| OuterMeasureClass.measure_mono ℙ hL⟩
+  · specialize h t ⟨ht, lt_of_le_of_lt (zero_le r) hr⟩ (Ioi r) _
+    · use r, hr
+    · obtain ⟨K, ⟨hK2, hK1⟩, hK3⟩ := h
+      exact ⟨K, hK1, hK2, hK3 fun ⦃a⦄ a => a⟩
+
+end innerRegular
+
+
 section tightMeasureSet
 
 variable {α : Type*} [TopologicalSpace α] [MeasurableSpace α] (ℙ : Measure α) [IsProbabilityMeasure ℙ]
 
-def IsTightMeasureSet (S : Set (Measure α)) := Tendsto (fun A ↦ ⨆ ℙ ∈ S, ℙ Aᶜ) (atTopOf IsCompact) (nhds 0)
+def IsTightMeasureSet (S : Set (Measure α)) := Tendsto (fun A ↦ ⨆ ℙ ∈ S, ℙ Aᶜ) (atTopAbove IsCompact) (nhds 0)
 
 lemma isTightMeasureSet_iff {S : Set (Measure α)} :
     IsTightMeasureSet S ↔ ∀ ε > 0, ∃ K : Set α, IsCompact K ∧ ∀ ℙ ∈ S, ℙ Kᶜ ≤ ε := by
