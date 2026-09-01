0d0bd6b1c65e7d4822bcffb723a44f079ba6c398
pfaffelh <pfaffelh@gmail.com>
2026-09-01 23:19:44 +0200

    Task23: leere Debugdatei entfernt (ihr Befund steht in neighbor.py)

--- Journal/Blog/MartingaleProblem/Task23/debug_neighbor.py
+++
@@ -1,3 +0,0 @@
-# Leer.  Rest eines Debughilfsskripts des vierzehnten Laufs (2026-09-01);
-# kann geloescht werden.  Der Befund, den es fand, steht in neighbor.py
-# (der Blockaufbau zaehlte Bloecke statt Punkte) und im PROTOKOLL.
